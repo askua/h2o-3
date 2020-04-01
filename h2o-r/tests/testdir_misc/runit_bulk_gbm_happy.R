20473f4767d8cec8e5ef1a0ba87c2d0a47123472
michalkurka <michalk@h2o.ai>
2020-04-01 10:08:34 -0400

    First take at rewriting the API

--- h2o-r/tests/testdir_misc/runit_bulk_gbm_happy.R
+++ h2o-r/tests/testdir_misc/runit_bulk_gbm_happy.R
@@ -7,10 +7,11 @@ test.bulk_gbm <- function() {
     smtrees <- h2o.importFile(locate("smalldata/gbm_test/smtrees.csv"))
     smtrees$segment <- as.factor(smtrees$C1 < 6)
 
-    segment_models <- h2o.bulk_gbm(x=c("girth", "height"), y="vol", ntrees=3, max_depth=1, seed=42,
-                                   distribution="gaussian", min_rows=2,
-                                   training_frame=smtrees, segment_columns="segment", segment_models_id = "smtrees_sm")
-    
+    segment_models <- h2o.segment_train(x=c("girth", "height"), y="vol", ntrees=3, max_depth=1, seed=42,
+                                        distribution="gaussian", min_rows=2,
+                                        training_frame=smtrees, segment_columns="segment", segment_models_id = "smtrees_sm",
+                                        algorithm="gbm")
+
     expect_equal(segment_models@segment_models_id, "smtrees_sm")
 
     segment_models_df <- as.data.frame(segment_models)
